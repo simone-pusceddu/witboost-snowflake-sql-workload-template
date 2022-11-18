1e61b2de7593b520fd26bd381ead35389098fc96
Sergio Andres Mejia Tovar <sergio.mejia@agilelab.it>
2022-11-18 15:06:08 +0000

    [#2] Resolve "Add a default valid SQL"
    # New features and improvements
    
    * Adds a SQL script to push by default
    
    # Related issue
    
    Closes #2

--- skeleton/artifacts/snowflake.sql
+++ skeleton/artifacts/snowflake.sql
@@ -1,1 +1,14 @@
-select * from mihailotry
+insert into output_table(
+    date,
+    location_key,
+    new_confirmed,
+    new_deceased,
+    new_recovered,
+    new_tested,
+    cumulative_confirmed,
+    cumulative_deceased,
+    cumulative_recovered,
+    cumulative_tested
+) select date, location_key, new_confirmed, new_deceased, new_recovered, new_tested,
+            cumulative_confirmed, cumulative_deceased, cumulative_recovered, cumulative_tested 
+from google_covid_dataset;
