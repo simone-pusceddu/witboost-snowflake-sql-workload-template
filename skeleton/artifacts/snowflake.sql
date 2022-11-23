2284356056d8af7c9802c262966f422a6a102084
Mihailo Vlajkovic <mihailo.vlajkovic@agilelab.it>
2022-11-23 10:59:59 +0000

    Update skeleton/artifacts/snowflake.sql, skeleton/catalog-info.yaml

--- skeleton/artifacts/snowflake.sql
+++ skeleton/artifacts/snowflake.sql
@@ -1,4 +1,4 @@
-insert into output_table(
+insert into ${{ values.domain }}.${{ values.identifier.split(".")[1] }}_${{ values.identifier.split(".")[2] }}.output_table(
     date,
     location_key,
     new_confirmed,
@@ -11,4 +11,4 @@ insert into output_table(
     cumulative_tested
 ) select date, location_key, new_confirmed, new_deceased, new_recovered, new_tested,
             cumulative_confirmed, cumulative_deceased, cumulative_recovered, cumulative_tested 
-from google_covid_dataset;
+from ${{ values.domain }}.${{ values.identifier.split(".")[1] }}_${{ values.identifier.split(".")[2] }}.google_covid_dataset;
