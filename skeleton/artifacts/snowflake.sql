26f6a720c9dfb09e2362859b40a292df7b729eb4
Sahith Yerram <sahith.yerram@agilelab.it>
2022-12-21 13:43:59 +0530

    Revert ""modify the table name""
    This reverts commit 667b21e8a99deb15314764be35bd36c6a62c17e3.

--- skeleton/artifacts/snowflake.sql
+++ skeleton/artifacts/snowflake.sql
@@ -11,4 +11,4 @@ insert into ${{ values.identifier.split(".")[0] }}.${{ values.identifier.split("
     cumulative_tested
 ) select date, location_key, new_confirmed, new_deceased, new_recovered, new_tested,
             cumulative_confirmed, cumulative_deceased, cumulative_recovered, cumulative_tested 
-from ${{ values.identifier.split(".")[0] }}.${{ values.identifier.split(".")[1].split("-").join("") }}_${{ values.identifier.split(".")[2] }}.Vaccinations_raw;
+from ${{ values.identifier.split(".")[0] }}.${{ values.identifier.split(".")[1].split("-").join("") }}_${{ values.identifier.split(".")[2] }}.google_covid_dataset;
